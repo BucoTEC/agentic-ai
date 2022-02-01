79988df42a7a7d559c22ead63d77b93c2dece27c
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-01 22:59:06 +0100

    made basic routing setup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import auth from "./routes/auth.js";
+import users from "./routes/users.js";
+import bookings from "./routes/bookings.js";
 
 dotenv.config();
 const app = express();
@@ -12,6 +15,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json("Evrirtihing is working ok");
 });
+
+app.use("/auth", auth);
+app.use("/users", users);
+app.use("/bookings", bookings);
 
 // startup
 const port = process.env.PORT || 5500;
