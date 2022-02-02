fc7e6d745b13769a7461f740df46432b80a72546
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-02 21:04:41 +0100

    added basic user login

---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+import User from "../models/user.js";
+import "express-async-errors";
+
+export const getAllUsers = async (req, res) => {
+  const allUsers = await User.find();
+  if (allUsers.length < 1) {
+    throw new Error("No users in database");
+  }
+  res.json(allUsers);
+};
+
+export const creatUser = async (req, res) => {
+  const newUser = await new User(req.body);
+  await newUser.save();
+
+  res.json(newUser);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,9 @@
 import express from "express";
 const router = express.Router();
-import User from "../models/user.js";
-import "express-async-errors";
+import { getAllUsers, creatUser } from "../controllers/users.js";
 
-router.get("/", async (req, res) => {
-  const allUsers = await User.find();
-  if (allUsers.length < 1) {
-    throw new Error("No users in database");
-  }
-  res.json(allUsers);
-});
+router.get("/", getAllUsers);
 
-router.post("/", async (req, res) => {
-  if (!req.body) {
-    throw new Error("Must include payload in req body");
-  }
-  const newUser = await new User(req.body);
-  await newUser.save();
-
-  res.json(newUser);
-});
+router.post("/", creatUser);
 
 export default router;
