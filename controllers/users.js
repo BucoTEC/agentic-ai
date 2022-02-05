97a8de1defd95bca1f7fd1c34aa9ebaf92f1d151
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-05 15:17:22 +0100

    added password encription

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import "express-async-errors";
+import bcrypt from "bcrypt";
 
 export const getAllUsers = async (req, res) => {
   const allUsers = await User.find();
@@ -10,7 +11,19 @@ export const getAllUsers = async (req, res) => {
 };
 
 export const creatUser = async (req, res) => {
-  const newUser = await new User(req.body);
+  const { username, email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    console.log(!!existingUser);
+    throw Error("User already exists");
+  }
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
   await newUser.save();
 
   res.json(newUser);
@@ -31,4 +44,8 @@ export const deleteUser = async (req, res) => {
   res.json("Deleted user succesfuly");
 };
 
+// export const updateUser =  async(req,res)=>{
+//   const {id} = req.params
+//   const updatedUser
+// }
 //TODO need to add bycrip for passeord encription and JWT for auth
