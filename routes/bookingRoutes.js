a9e4172acc3d24a4ca17b9676850944e4fdf7d08
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-04-21 22:42:35 +0200

    setup basic booking routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,7 +2,22 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json("this is the booking route");
+	res.json("get all bookings");
 });
 
+router.get("/:id", (req, res) => {
+	res.json("find one booking ");
+});
+
+router.post("/", (req, res) => {
+	res.json("create booking");
+});
+
+router.patch("/:id", (req, res) => {
+	res.json("update booking");
+});
+
+router.delete("/:id", (req, res) => {
+	res.json("delete one booking");
+});
 export default router;
