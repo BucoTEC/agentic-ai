28ef026de5abab833fb2203bc62adf74e985885e
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-04-28 00:12:40 +0200

    add verification email helper function

---
+++ utils/sendVerificationMail.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+const sendVerificationMail = () => {
+	const transporter = nodemailer.createTransport({
+		service: "hotmail",
+		auth: {
+			user: "test040120004716@outlook.com",
+			pass: "0401Adnan",
+		},
+	});
+
+	const options = {
+		from: "test040120004716@outlook.com",
+		to: "adnan.bucalovic13@gmail.com",
+		subject: "email from booking api",
+		text: "hello from booking api",
+	};
+
+	transporter.sendMail(options, (err, info) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		console.log("Sent email: " + info.response);
+	});
+};
+
+export default sendVerificationMail;
