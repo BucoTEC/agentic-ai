455da4e01dda8266994a4ce0da6ac111f8da7c38
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-04-28 01:03:14 +0200

    add confirm register route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
-import { singIn, register } from "../controllers/authControllers.js";
+import {
+	singIn,
+	register,
+	confirmRegister,
+} from "../controllers/authControllers.js";
 
 router.get("/", singIn);
+router.get("/confirm/:token", confirmRegister);
 router.post("/", register);
 
 export default router;
