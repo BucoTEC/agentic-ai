f5032707d6e5044f11df69f952aa1cfb38b7c360
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-05-01 12:22:28 +0200

    add first data validatin

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,18 @@ import {
 	confirmRegister,
 } from "../controllers/authControllers.js";
 
-router.get("/", singIn);
+import { body, validationResult } from "express-validator";
+
+const errorResponse = (req, res, next) => {
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+	next();
+};
+
+router.get("/", body("email").isEmail(), errorResponse, singIn);
 router.get("/confirm/:token", confirmRegister);
 router.post("/", register);
 
